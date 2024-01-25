61c359161b47cf5ac318e3ff1cd9c9a0f27a6e54
MrRelaxedAsh <ashrafulalamshuvo920@gmail.com>
2024-01-25 10:08:10 +0600

    commit

--- basics.c
+++ basics.c
@@ -2,4 +2,15 @@
 int main()
 {
     int a;
+    int *pa;
+    a = 24;
+    pa = &a;
+    printf("value of a = %d\n",a);
+    printf("address of a in 'p' %p\n",&a);
+    printf("address of a in 'p' a = %p\n",pa);
+    printf("value that is located in the memory address of pa = %d\n",*pa);
+
+    /* here pa (pointer of a) is indicating the value which is assigned in the address of a means pa-->&a 
+       pointer*/
+
 }
